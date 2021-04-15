ee3f1e7f60e04967aa8fc4946bc8bd6d6fa5f051
Felipe Ribeiro <piporibeiro@gmail.com>
2021-04-14 21:03:15 -0300

    feat: create catalog and reducer

--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -1,10 +1,27 @@
-import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import api from "../services/api";
+import { IProduct } from "../store/modules/cart/types";
 
 const Catalog = () => {
-  const catalog = useSelector((state) => state);
+  const [catalog, setCatalog] = useState<IProduct[]>([]);
 
-  console.log(catalog);
-  return <h1>Catalog</h1>;
+  useEffect(() => {
+    api.get("products").then((response) => {
+      setCatalog(response.data);
+    });
+  }, []);
+
+  return (
+    <main>
+      <h1>Catalog</h1>
+      {catalog.map((product) => (
+        <article key={product.id}>
+          <strong>{product.title}</strong> {" - "}
+          <span>{product.price}</span> <button type="button">Comprar</button>
+        </article>
+      ))}
+    </main>
+  );
 };
 
 export default Catalog;
