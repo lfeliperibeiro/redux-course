cf0a81b3d686fff027584517561a9b52f7de32f8
Felipe Ribeiro <piporibeiro@gmail.com>
2021-04-18 21:51:32 -0300

    feat: create conditional for failure check

--- src/store/modules/cart/reducer.ts
+++ src/store/modules/cart/reducer.ts
@@ -1,15 +1,16 @@
 import { Reducer } from "redux";
 import produce from "immer";
-import { ICartState } from "./types";
+import { ActionTypes, ICartState } from "./types";
 
 const INITIAL_STATE: ICartState = {
   items: [],
+  failureStockCheck: [],
 };
 
 const cart: Reducer<ICartState> = (state = INITIAL_STATE, action) => {
   return produce(state, (draft) => {
     switch (action.type) {
-      case "ADD_PRODUCT_TO_CART": {
+      case ActionTypes.addProductToCartSuccess: {
         const { product } = action.payload;
         const productInCartIndex = draft.items.findIndex(
           (item) => item.product.id === product.id
@@ -23,6 +24,10 @@ const cart: Reducer<ICartState> = (state = INITIAL_STATE, action) => {
             quantity: 1,
           });
         }
+        break;
+      }
+      case ActionTypes.addProductToCartFailure: {
+        draft.failureStockCheck.push(action.payload.productId);
         break;
       }
       default: {
